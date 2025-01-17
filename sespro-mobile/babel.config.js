51437b32fdb71a51469a06c0ce7c78116fb27ab5
xmanguji <xmanguji@outlook.com>
2025-01-17 17:23:00 +0800

    Update babel.config.js

--- sespro-mobile/babel.config.js
+++ sespro-mobile/babel.config.js
@@ -3,3 +3,61 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+
+// API_ROOT_PATH=/staging
+// API_TIMEZONE=America/Phoenix
+// API_TITLE=Report API Staging
+// AWS_S3_BUCKET=mm-report-api-staging
+// AWS_S3_DATA_FOLDER=raw_data/
+// AWS_S3_IMAGE_FOLDER=image/
+// AWS_S3_REMAP_FOLDER=remaps/
+// AWS_SES_SENDER=claire@methodmill.com
+// CLIENT_BASE_URL=localhost
+// DEV_DB_HOST=mm-apps.cybqo5uzswvh.us-east-1.rds.amazonaws.com
+// DEV_DB_NAME=report_api_staging
+// DEV_DB_PASS=sadf3x33$za
+// DEV_DB_USER=report_api_staging
+// EMAILER_LAMBDA=report-emailer-staging
+// PDF_LAMBDA=report-docprinter-staging
+// #PDF_LAMBDA=respro-docprinter-prod
+// PDF_FOLDER=https://mm-report-api-staging.s3.amazonaws.com/
+// ENVIRONMENT=DEVELOPMENT
+// ERROR_EMAILS=["taylor@methodmill.com", "claire@methodmill.com"]
+// INCLUDE_NA=True
+// INCLUDE_PASS=True
+// #RENDER_BASE_URL=https://dzyy41lvyg.execute-api.us-east-1.amazonaws.com/staging/v1_0
+// RENDER_BASE_URL=http://localhost:8000/v2_0
+// RENDER_TEMPLATE=respro.html
+// SECRET=BqUv8Q
+// SEND_TO_AUDITOR=True
+
+
+// ===================================================
+
+// API_ROOT_PATH=/prod
+// API_TIMEZONE=America/Phoenix
+// API_TITLE=Respro
+// AWS_S3_BUCKET=mm-respro-prod
+// AWS_S3_DATA_FOLDER=raw_data/
+// AWS_S3_IMAGE_FOLDER=image/
+// AWS_S3_REMAP_FOLDER=remaps/
+// AWS_SES_SENDER=claire@methodmill.com
+// CLIENT_BASE_URL=localhost
+// PROD_DB_HOST=mm-apps.cybqo5uzswvh.us-east-1.rds.amazonaws.com
+// PROD_DB_NAME=respro_prod
+// PROD_DB_PASS=eezf7(322fd44$ae#s
+// PROD_DB_USER=respro_prod
+// EMAILER_LAMBDA=respro-emailer-prod
+// PDF_LAMBDA=report-docprinter-staging
+// #PDF_LAMBDA=respro-docprinter-prod
+// PDF_FOLDER=https://mm-report-api-staging.s3.amazonaws.com/
+// ENVIRONMENT=PRODUCTION
+// ERROR_EMAILS=["taylor@methodmill.com", "claire@methodmill.com"]
+// INCLUDE_NA=True
+// INCLUDE_PASS=True
+// #RENDER_BASE_URL=https://dzyy41lvyg.execute-api.us-east-1.amazonaws.com/staging/v1_0
+// RENDER_BASE_URL=http://localhost:8000/v2_0
+// RENDER_TEMPLATE=respro.html
+// SECRET=KRgc68
+// SEND_TO_AUDITOR=True
